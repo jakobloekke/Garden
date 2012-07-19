878987181ef1ff30334b69be27aa8e6541ba1606
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2012-07-19 20:06:59 +0200

    Added empty hooks

--- themes/lydmaskinen_v2/class.lydmaskinenthemehooks.php
+++ themes/lydmaskinen_v2/class.lydmaskinenthemehooks.php
@@ -18,9 +18,15 @@ class Lydmaskinen_v2ThemeHooks implements Gdn_IPlugin
     }
 
     public function Base_Render_Before($Sender) {
-
+        return true;
     }
 
+    public function DiscussionController_AfterBuildPager_Handler($Sender) {
+        return true;
+    }
 
+    public function Controller_AfterJsCdns_Handler($sender) {
+        return true;
+    }
 
 }
